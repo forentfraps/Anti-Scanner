cecd610c83a2ef1f8369d4bb3da367fd482480d2
The One The Master <vacpox@gmail.com>
2024-04-05 02:06:03 +0100

    1.0 working in win

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use core::net::Ipv4Addr;
 use pcap;
 use pnet::datalink::{self, NetworkInterface};
 use pnet::packet::ethernet::EtherTypes;
@@ -7,7 +8,102 @@ use pnet::packet::ipv4::Ipv4Packet;
 use pnet::packet::tcp::TcpPacket;
 use pnet::packet::udp::UdpPacket;
 use pnet::packet::Packet;
+use std::collections::{HashMap, HashSet};
 use std::env;
+use std::process::Command;
+
+const ALLOWED_AMOUNT_OF_PORTS: usize = 25;
+
+fn block_ip_win(ip: &str) {
+    // Construct the netsh command to add a firewall rule
+    let output = Command::new("netsh")
+        .args(&[
+            "advfirewall",
+            "firewall",
+            "add",
+            "rule",
+            "name=AntiScanner Blocked IP TCP", // Unique name for the rule
+            &format!("remoteip={}", ip),       // The IP address to block
+            "dir=in",                          // Direction: in = inbound, out = outbound
+            "action=block",                    // Block the matching traffic
+            "protocol=TCP", // Specify the protocol (optional, based on your needs)
+            "enable=yes",   // Enable the rule
+        ])
+        .output()
+        .expect("Failed to execute command");
+
+    if output.status.success() {
+        println!("IP {} successfully blocked TCP.", ip);
+    } else {
+        // Print the error output from the command for debugging
+        eprintln!(
+            "Error blocking IP {}: {}",
+            ip,
+            String::from_utf8_lossy(&output.stderr)
+        );
+    }
+    let output = Command::new("netsh")
+        .args(&[
+            "advfirewall",
+            "firewall",
+            "add",
+            "rule",
+            "name=AntiScanner Blocked IP UDP", // Unique name for the rule
+            &format!("remoteip={}", ip),       // The IP address to block
+            "dir=in",                          // Direction: in = inbound, out = outbound
+            "action=block",                    // Block the matching traffic
+            "protocol=UDP", // Specify the protocol (optional, based on your needs)
+            "enable=yes",   // Enable the rule
+        ])
+        .output()
+        .expect("Failed to execute command");
+
+    if output.status.success() {
+        println!("IP {} successfully blocked UDP.", ip);
+    } else {
+        // Print the error output from the command for debugging
+        eprintln!(
+            "Error blocking IP {}: {}",
+            ip,
+            String::from_utf8_lossy(&output.stderr)
+        );
+    }
+}
+fn block_ip_unix(ip: &str) {
+    let status = Command::new("sudo")
+        .arg("iptables")
+        .arg("-A")
+        .arg("INPUT")
+        .arg("-s")
+        .arg(ip)
+        .arg("-j")
+        .arg("DROP")
+        .status()
+        .expect("Failed to execute iptables command");
+
+    if status.success() {
+        println!("Successfully blocked IP {}", ip);
+    } else {
+        eprintln!("Failed to block IP {}", ip);
+    }
+}
+fn block_ip(ip: &str) {
+    #[cfg(target_family = "unix")]
+    {
+        block_ip_unix(ip)
+    }
+
+    #[cfg(target_os = "windows")]
+    {
+        block_ip_win(ip)
+    }
+
+    #[cfg(not(any(target_family = "unix", target_os = "windows")))]
+    {
+        false // Or some default behavior for other platforms
+    }
+}
+
 #[cfg(target_family = "unix")]
 fn has_root_permissions() -> bool {
     nix::unistd::geteuid().is_root()
@@ -63,6 +159,27 @@ fn has_elevated_permissions() -> bool {
         false // Or some default behavior for other platforms
     }
 }
+
+fn catalog_packet(
+    hm: &mut HashMap<Ipv4Addr, HashSet<u16>>,
+    hs: &mut HashSet<Ipv4Addr>,
+    ip: &Ipv4Addr,
+    port: u16,
+) {
+    let mut ports_list = match hm.get_mut(ip) {
+        Some(vec) => vec,
+        None => {
+            hm.insert(ip.clone(), HashSet::new());
+            hm.get_mut(ip).unwrap()
+        }
+    };
+    ports_list.insert(port);
+    if ports_list.len() > ALLOWED_AMOUNT_OF_PORTS && !hs.contains(ip) {
+        block_ip(&(ip.to_string()));
+        hs.insert(ip.clone());
+    }
+}
+
 fn fail_start() -> ! {
     let interfaces = datalink::interfaces();
     println!("Please provide a valid interface index from this list:");
@@ -101,6 +218,7 @@ fn main() {
 
     // Create a new channel to capture packets
     let interface_name: &str = &(interface.name);
+    let host_ip = interface.ips[0].ip();
     let mut cap = match pcap::Capture::from_device(interface_name)
         .unwrap()
         .promisc(true) // Set the capture mode to promiscuous
@@ -113,6 +231,8 @@ fn main() {
             std::process::exit(0);
         }
     };
+    let mut hs = HashSet::new();
+    let mut hm = HashMap::new();
     while let Ok(packet) = cap.next() {
         // Parse the Ethernet frame from the captured packet data
         if let Some(ethernet_packet) = EthernetPacket::new(&packet.data) {
@@ -125,39 +245,49 @@ fn main() {
                                 // Handle TCP packets
                                 let tcp_packet = TcpPacket::new(ipv4_packet.payload());
                                 if let Some(tcp_packet) = tcp_packet {
-                                    println!(
-                                        "TCP Packet: {}:{} > {}:{}; Seq: {}, Ack: {}",
-                                        ethernet_packet.get_source(),
-                                        tcp_packet.get_source(),
-                                        ethernet_packet.get_destination(),
-                                        tcp_packet.get_destination(),
-                                        tcp_packet.get_sequence(),
-                                        tcp_packet.get_acknowledgement()
-                                    );
+                                    // println!(
+                                    //     "TCP Packet: From {} To {} {}:{} > {}:{}; Seq: {}, Ack: {}",
+                                    //     ipv4_packet.get_source(),
+                                    //     ipv4_packet.get_destination(),
+                                    //     ethernet_packet.get_source(),
+                                    //     tcp_packet.get_source(),
+                                    //     ethernet_packet.get_destination(),
+                                    //     tcp_packet.get_destination(),
+                                    //     tcp_packet.get_sequence(),
+                                    //     tcp_packet.get_acknowledgement()
+                                    // );
+                                    if ipv4_packet.get_destination() == host_ip {
+                                        catalog_packet(
+                                            &mut hm,
+                                            &mut hs,
+                                            &ipv4_packet.get_source(),
+                                            tcp_packet.get_source(),
+                                        )
+                                    }
                                 }
                             }
                             IpNextHeaderProtocols::Udp => {
                                 // Handle UDP packets
                                 let udp_packet = UdpPacket::new(ethernet_packet.payload());
                                 if let Some(udp_packet) = udp_packet {
-                                    println!(
-                                        "UDP Packet: {}:{} > {}:{}; Len: {}",
-                                        ethernet_packet.get_source(),
-                                        udp_packet.get_source(),
-                                        ethernet_packet.get_destination(),
-                                        udp_packet.get_destination(),
-                                        udp_packet.get_length()
-                                    );
+                                    if ipv4_packet.get_destination() == host_ip {
+                                        catalog_packet(
+                                            &mut hm,
+                                            &mut hs,
+                                            &ipv4_packet.get_source(),
+                                            udp_packet.get_source(),
+                                        );
+                                    }
                                 }
                             }
                             _ => {
-                                println!("protocol is {:?}", ipv4_packet.get_next_level_protocol());
+                                //println!("protocol is {:?}", ipv4_packet.get_next_level_protocol());
                             }
                         }
                     }
                 }
                 _ => {
-                    println!("ether type is {:?}", ethernet_packet.get_ethertype())
+                    // println!("ether type is {:?}", ethernet_packet.get_ethertype())
                 }
             }
         }
